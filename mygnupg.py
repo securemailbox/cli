b65a6f1fb60c96d0ded8066bd689460771bfb9a7
xingchengyusi <zsxxxzks1314@gmail.com>
2020-02-24 19:54:27 -0800

    fix problem

--- mygnupg.py
+++ mygnupg.py
@@ -1,33 +1,31 @@
 import gnupg
+from pathlib import Path
+from os import getlogin
+from random import randint
 
-class mygnupg():
+class GpgOpt():
     """
     This class used for initial gnupg, 
     create, import, and export key pairs.
     Also, can encrypt and decrypt data.
     """
-    def __init__(self):
+    def __init__(self, password, mygnupghome='gnupgkeys', email=os.getlogin()+'@', key_type="RSA"):
         """
-        The binary file should be different
-        in different os.
-        This is MacOS version.
         """
-        self.gpg = gnupg.GPG(gnupghome='./gnupgkeys')
+        p = Path(mygnupghome)
+        self.gpg = gnupg.GPG(gnupghome=mygnupghome.name)
         self.key = None
-        self.email = "john_smith@scmail.org"
-        self.keytype = "RSA"
-        self.password = "sample"
+        self.email = email
+        self.key_type = key_type
+        self.password = password
 
     def import_key(self, path):
         """
         This function used for check whether the gnupg pub or pvt key already exist.
         If exist, import it, if not, return false.
         """
-        try:
-            key_data = open(path).read()
-            self.key = self.gpg.import_keys(key_data)
-        except:
-            raise Exception('the file is wrong or not exist!')
+        key_data = open(path).read()
+        self.key = self.gpg.import_keys(key_data)
 
     def create(self):
         """
@@ -39,15 +37,16 @@ class mygnupg():
         # gen key pair
         self.key = self.gpg.gen_key(input_data)
 
-    def export_key(self, tofile=False):
+    def export_key(self, to_file=False, path=None):
         sc_pub = self.gpg.export_keys(self.key.fingerprint)
         sc_pvt = self.gpg.export_keys(self.key.fingerprint, True, passphrase=self.password)
-        # print(sc_pvt)
-        if tofile == False:
+        print(sc_pub, sc_pvt)
+
+        if to_file == False:
             return
 
         # store key for test
-        with open('./jskey.asc', 'w') as f:
+        with open(path, 'w') as f:
             f.write(sc_pub)
             f.write(sc_pvt)
         # print('Successful generate a new key.')
@@ -56,9 +55,9 @@ class mygnupg():
         return self.gpg.list_keys(sec)
 
 if __name__ == "__main__":
-    mygpg = mygnupg()
+    mygpg = GpgOpt()
+    mygpg.create()
+    # mygpg.export_key()
+    mygpg.export_key(True, './jskey.asc')
     mygpg.import_key('./jskey.asc')
     a = mygpg.list()
-    b = mygpg.list(True)
-    print(a, '\n', b)
-    # gpg.create()
